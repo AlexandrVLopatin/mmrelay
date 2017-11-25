2b7701277b3e452592e828374e8523a9096bda7e
Alexandr Lopatin <alexandr.lopatin@convergent-usa.com>
2017-11-25 19:45:44 +0400

    add async dns resolver

--- mmrelay.py
+++ mmrelay.py
@@ -5,6 +5,7 @@ import sys
 
 import asyncio
 import aiohttp
+from aiohttp.resolver import AsyncResolver
 import requests
 import websockets
 
@@ -13,7 +14,7 @@ MM_WSURL = 'ws://127.0.0.1:8065/api/v4/websocket'
 MM_LOGIN = 'bot@localhost.local'
 MM_PASSWORD = '12345678'
 
-CALLBACK_URL = 'https://api.myapp.com/messages'
+CALLBACK_URL = 'http://127.0.0.1/1.php'
 CALLBACK_TIMEOUT = 10
 
 
@@ -53,33 +54,39 @@ async def authenticateWebsocket(websocket):
     response = await websocket.recv()
     status = json.loads(response)
     if 'event' in status and status['event'] == 'hello':
+        print("Auth OK")
         return True
     else:
         return False
 
 
 async def waitForMessage(websocket):
+    print("Waiting for messages")
     while True:
         event = await websocket.recv()
         await eventHandler(event)
 
 
 async def eventHandler(event):
+    print(event)
     data = json.loads(event)
     if 'event' in data and data['event'] == 'posted':
         loop.create_task(sendCallback(event))
 
 
 async def sendCallback(event):
+    conn = aiohttp.TCPConnector(resolver=dnsResolver)
     try:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(connector=conn, loop=loop) as session:
             await session.post(CALLBACK_URL, json=event, timeout=CALLBACK_TIMEOUT)
+            await session.close()
     except asyncio.TimeoutError:
         pass
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+    dnsResolver = AsyncResolver()
 
     token = login()
 
